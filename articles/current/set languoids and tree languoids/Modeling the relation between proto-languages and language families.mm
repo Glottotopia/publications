bc96058e36be39e3fa532e7fcf72b1b76dbdaba9
Schmalhans <lqfb@glottotopia.de>
2015-12-04 11:09:59 +0100

    current

---
+++ articles/current/set languoids and tree languoids/Modeling the relation between proto-languages and language families.mm
@@ -0,0 +1,177 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1319471850264" ID="Freemind_Link_954314275" MODIFIED="1319471883786" TEXT="Modeling the relation between &#xa;proto-languages and language families">
+<node CREATED="1319471888400" ID="_" MODIFIED="1319471894868" POSITION="right" TEXT="intro">
+<node CREATED="1319471896183" ID="Freemind_Link_1870036149" MODIFIED="1319471900194" TEXT="language classifications">
+<node CREATED="1319471901453" ID="Freemind_Link_856376227" MODIFIED="1319471915892" TEXT="Ethnologue"/>
+<node CREATED="1319471907364" ID="Freemind_Link_1039257246" MODIFIED="1319471912149" TEXT="Multitree Composite">
+<node CREATED="1319472976455" MODIFIED="1319472976455" TEXT="Indo-European"/>
+<node CREATED="1319472976455" MODIFIED="1319472976455" TEXT="Middle Indo-European"/>
+<node CREATED="1319472976455" MODIFIED="1319472976455" TEXT="Late Indo-European"/>
+<node CREATED="1319472976456" MODIFIED="1319472976456" TEXT="Germanic"/>
+<node CREATED="1319472976457" MODIFIED="1319472976457" TEXT="Northwest Germanic"/>
+<node CREATED="1319472976457" MODIFIED="1319472976457" TEXT="West Germanic"/>
+<node CREATED="1319472976457" MODIFIED="1319472976457" TEXT="North Sea Germanic"/>
+<node CREATED="1319472976457" MODIFIED="1319472976457" TEXT="Old Englishlanguage ISO 639-3"/>
+<node CREATED="1319472976457" MODIFIED="1319472976457" TEXT="Anglian"/>
+<node CREATED="1319472976457" MODIFIED="1319472976457" TEXT="Mercian"/>
+<node CREATED="1319472976458" MODIFIED="1319472976458" TEXT="Middle Englishlanguage ISO 639-3  English"/>
+</node>
+</node>
+<node CREATED="1319471919742" ID="Freemind_Link_528153269" MODIFIED="1319471930638" TEXT="English ChildOf Germanic"/>
+<node CREATED="1319471930903" ID="Freemind_Link_1664805713" MODIFIED="1319471938832" TEXT="English ChildOf Old English"/>
+<node CREATED="1319471940540" ID="Freemind_Link_875372110" MODIFIED="1319471957631" TEXT="two important insights about the position of English in a network of languoids"/>
+<node CREATED="1319471961360" ID="Freemind_Link_1536024966" MODIFIED="1319471967765" TEXT="but different insights">
+<node CREATED="1319471970675" ID="Freemind_Link_1492192221" MODIFIED="1319471980116" TEXT="-&gt; should be made explicit"/>
+</node>
+<node CREATED="1319472416891" ID="Freemind_Link_1135978721" MODIFIED="1319472426109" TEXT="use stammbaum model as customary"/>
+</node>
+<node CREATED="1319471999759" ID="Freemind_Link_1286287917" MODIFIED="1319472006106" POSITION="right" TEXT="Resource based approach">
+<node CREATED="1319474271051" ID="Freemind_Link_390219754" MODIFIED="1319474276218" TEXT="only things we have resources for"/>
+<node CREATED="1319474299700" ID="Freemind_Link_1295080514" MODIFIED="1319474304677" TEXT="scientific method"/>
+<node CREATED="1319474276472" ID="Freemind_Link_734414504" MODIFIED="1319474281826" TEXT="no fiat languages"/>
+<node CREATED="1319472026486" ID="Freemind_Link_1014990655" MODIFIED="1319472055668" TEXT="there are languages outside of this definition, but they do not fall within the scientific realm"/>
+<node CREATED="1319473289415" ID="Freemind_Link_1259417981" MODIFIED="1319473301362" TEXT="Good, Nordhoff &amp; Hammarstr&#xf6;m ISWC">
+<node CREATED="1319473304824" ID="Freemind_Link_826172965" MODIFIED="1319473313426" TEXT="problems with fiat languages"/>
+<node CREATED="1319473316079" ID="Freemind_Link_152523273" MODIFIED="1319473322333" TEXT="--&gt; resource based approach"/>
+</node>
+<node CREATED="1319472007346" ID="Freemind_Link_9959931" MODIFIED="1319472010063" TEXT="empirical"/>
+<node CREATED="1319472010759" ID="Freemind_Link_1382374167" MODIFIED="1319472013675" TEXT="set theoretic">
+<node CREATED="1319473336871" ID="Freemind_Link_1434837223" MODIFIED="1319473345678" TEXT="in the domain of academic linguistics:"/>
+<node CREATED="1319472015415" ID="Freemind_Link_1605073632" MODIFIED="1319472023483" TEXT="a language is the sum of what we know about it"/>
+</node>
+</node>
+<node CREATED="1319471981317" ID="Freemind_Link_664081958" MODIFIED="1319471985562" POSITION="right" TEXT="definitions">
+<node CREATED="1319474331717" ID="Freemind_Link_648119834" MODIFIED="1319474333791" TEXT="overview">
+<node CREATED="1319474335442" ID="Freemind_Link_1926518589" MODIFIED="1319474337658" TEXT="lectodoc"/>
+<node CREATED="1319474337970" ID="Freemind_Link_1107883426" MODIFIED="1319474340171" TEXT="doculect"/>
+<node CREATED="1319474340505" ID="Freemind_Link_1341629092" MODIFIED="1319474342090" TEXT="languoid"/>
+</node>
+<node CREATED="1319472068431" ID="Freemind_Link_432020345" MODIFIED="1319472071282" TEXT="lectodoc">
+<node CREATED="1319473376604" ID="Freemind_Link_160083615" MODIFIED="1319473405209" TEXT="a document which contains linguistically relevant information">
+<node CREATED="1319473406772" ID="Freemind_Link_383701137" MODIFIED="1319473409846" TEXT="structural"/>
+<node CREATED="1319473410255" ID="Freemind_Link_1877531403" MODIFIED="1319473415955" TEXT="sociodemographic">
+<node CREATED="1319473421246" ID="Freemind_Link_1065650384" MODIFIED="1319473425603" TEXT="number of speakers"/>
+<node CREATED="1319473425832" ID="Freemind_Link_573587260" MODIFIED="1319473432540" TEXT="physical geography"/>
+<node CREATED="1319473432800" ID="Freemind_Link_167444482" MODIFIED="1319473437959" TEXT="political geography"/>
+<node CREATED="1319473442539" ID="Freemind_Link_1547134255" MODIFIED="1319473444871" TEXT="religion"/>
+<node CREATED="1319473451341" ID="Freemind_Link_128498061" MODIFIED="1319473456302" TEXT="ethnographic information"/>
+<node CREATED="1319473456613" ID="Freemind_Link_1873621456" MODIFIED="1319473462199" TEXT="intelligibiility"/>
+</node>
+</node>
+</node>
+<node CREATED="1319472059310" ID="Freemind_Link_892612199" MODIFIED="1319472068199" TEXT="doculect">
+<node CREATED="1319473476104" ID="Freemind_Link_894238390" MODIFIED="1319473484547" TEXT="The variety described in one lectodoc"/>
+</node>
+<node CREATED="1319471987050" ID="Freemind_Link_347295422" MODIFIED="1319471988842" TEXT="languoid">
+<node CREATED="1319473502295" ID="Freemind_Link_949210143" MODIFIED="1319473505497" TEXT="terminal languoid">
+<node CREATED="1319473492659" ID="Freemind_Link_972999331" MODIFIED="1319473501765" TEXT="A collection of doculects"/>
+</node>
+<node CREATED="1319473508587" ID="Freemind_Link_974592540" MODIFIED="1319473514229" TEXT="nonterminal languoid">
+<node CREATED="1319473515406" ID="Freemind_Link_560846728" MODIFIED="1319473531817" TEXT="A collection of languoids "/>
+<node CREATED="1319473532902" ID="Freemind_Link_662090536" MODIFIED="1319473540165" TEXT="Can contain additionall doculects"/>
+</node>
+</node>
+</node>
+<node CREATED="1319472158506" ID="Freemind_Link_1103022844" MODIFIED="1319473547462" POSITION="right" TEXT="illustration">
+<node CREATED="1319472073920" ID="Freemind_Link_681458319" MODIFIED="1319472097510" TEXT="The definition of contemporary Tamil">
+<node CREATED="1319473584613" MODIFIED="1319473584613" TEXT="Andronov, Michail S.  (1960) Tamil&apos;skij jazyk"/>
+<node CREATED="1319473605027" MODIFIED="1319473605027" TEXT="Annamalai, E.  (2000) Lexical anaphors and pronouns in Tamil"/>
+<node CREATED="1319473612888" MODIFIED="1319473612888" TEXT="Asher, R. E.  (1985) Tamil"/>
+<node CREATED="1319473630185" MODIFIED="1319473630185" TEXT="Beythan, Hermann  (1943) Praktische Grammatik und &#xdc;bungsbuch der Tamilsprache"/>
+<node CREATED="1319473764119" MODIFIED="1319473764119" TEXT="Lehmann, Thomas  (1993) A grammar of modern Tamil"/>
+</node>
+<node CREATED="1319472098827" ID="Freemind_Link_994585755" MODIFIED="1319472105571" TEXT="The definition of Old Tamil">
+<node CREATED="1319473754550" MODIFIED="1319473754550" TEXT="Lehmann, Thomas  (1991) Grammatik des Alttamil: unter besonderer Ber&#xfc;cksichtigung der Cankam-Texte des Dichters Kapilar"/>
+<node CREATED="1319473787354" MODIFIED="1319473787354" TEXT="Steever, Sanford B.  (2008) Old Tamil"/>
+<node CREATED="1319473807325" MODIFIED="1319473807325" TEXT="Zvelebil, Kamil and Andronov, Michail S. and Navrozov, L.  (1967) Introduction to the historical grammar of the Tamil language: Part 1, Preliminary textual analysis [Vvedenie v istoriceskuju grammatiky tamilskogo jazyka &lt;engl.&gt;]"/>
+</node>
+<node CREATED="1319472105824" ID="Freemind_Link_994888240" MODIFIED="1319472115676" TEXT="The definition of Dravidian">
+<node CREATED="1319473877694" MODIFIED="1319473877694" TEXT="A comparative grammar of the Dravidian languages"/>
+<node CREATED="1319473892093" MODIFIED="1319473892093" TEXT="Bloch, Jules  (1954) The grammatical structure of Dravidian languages"/>
+<node CREATED="1319473901368" MODIFIED="1319473901368" TEXT="Burrow, Thomas and Emeneau, M. B.  (1998) A Dravidian etymological dictionary"/>
+<node CREATED="1319473912154" ID="Freemind_Link_1549614381" MODIFIED="1319474361378" TEXT="Caldwell, Robert  (1998) A comparative grammar of the Dravidian or South Indian family of languages"/>
+<node CREATED="1319473958512" MODIFIED="1319473958512" TEXT="K. V. Zvelebil  (1990) Dravidian Linguistics: An Introduction"/>
+<node CREATED="1319474029175" MODIFIED="1319474029175" TEXT="Rangaswamy, R.  (1995) Comparative Dravidian"/>
+</node>
+<node CREATED="1319472115908" ID="Freemind_Link_394461469" MODIFIED="1319472125384" TEXT="The definition of Proto-Dravidian">
+<node CREATED="1319474072130" ID="Freemind_Link_1220907882" MODIFIED="1319474072130" TEXT="William Bright  (1986) Archaeology, linguistics, and ancient Dravidian"/>
+<node CREATED="1319474011066" MODIFIED="1319474011066" TEXT="Levi, Sylvain and Przyluski, Jean and Block, Jules  (1929) Pre-Aryan and pre-Dravidian in India"/>
+<node CREATED="1319473926901" ID="Freemind_Link_455224054" MODIFIED="1319473926901" TEXT="D. McAlpin  (1974) Toward Proto-Elamo-Dravidian"/>
+<node CREATED="1319473997861" ID="Freemind_Link_1366158846" MODIFIED="1319473997861" TEXT="Lars-Gunnar Larsson  (1982) Some remarks on the hypothesis of an Uralo-Dravidian genetic linguistic relationship"/>
+</node>
+<node CREATED="1319474372760" ID="Freemind_Link_1603743546" MODIFIED="1319474414708" TEXT="Annamalai describes Tamil">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1319474384441" ID="Freemind_Link_1025006961" MODIFIED="1319474417080" TEXT="Annamalai descibes Old Tamil">
+<icon BUILTIN="button_cancel"/>
+</node>
+<node CREATED="1319474395691" ID="Freemind_Link_1946881658" MODIFIED="1319474418281" TEXT="Annamalai describes Dravidian">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1319474402606" ID="Freemind_Link_346984044" MODIFIED="1319474419384" TEXT="Annamalai describes Proto-Dravidian">
+<icon BUILTIN="button_cancel"/>
+</node>
+</node>
+<node CREATED="1319472261330" ID="Freemind_Link_478538087" MODIFIED="1319472265211" POSITION="right" TEXT="types of languoids">
+<node CREATED="1319472266315" ID="Freemind_Link_362183196" MODIFIED="1319472271216" TEXT="SetLangoid">
+<node CREATED="1319474108954" ID="Freemind_Link_346021854" MODIFIED="1319474116316" TEXT="defintion">
+<node CREATED="1319474120331" ID="Freemind_Link_250363517" MODIFIED="1319474167978" TEXT="can be in superset relation withanother languoid"/>
+</node>
+<node CREATED="1319472365165" ID="Freemind_Link_1795462675" MODIFIED="1319472378209" TEXT="SuperLanguoid"/>
+<node CREATED="1319472378816" ID="Freemind_Link_535911947" MODIFIED="1319472382043" TEXT="SubLanguoid"/>
+<node CREATED="1319472386679" ID="Freemind_Link_866412463" MODIFIED="1319472405818" TEXT="Scatter"/>
+<node CREATED="1319472395406" ID="Freemind_Link_1447381366" MODIFIED="1319472402373" TEXT="LatestCommonAncestor"/>
+</node>
+<node CREATED="1319472271690" ID="Freemind_Link_39638160" MODIFIED="1319472283584" TEXT="TreeLanguoid">
+<node CREATED="1319474171683" ID="Freemind_Link_874299322" MODIFIED="1319474174683" TEXT="definition">
+<node CREATED="1319474176510" ID="Freemind_Link_1198090329" MODIFIED="1319474188252" TEXT="cannot be in superset relation with another languoid"/>
+</node>
+<node CREATED="1319474189273" ID="Freemind_Link_1416971291" MODIFIED="1319474190981" TEXT="subtypes">
+<node CREATED="1319472525109" ID="Freemind_Link_350099319" MODIFIED="1319472531326" TEXT="Historic Variety">
+<node CREATED="1319472606018" ID="Freemind_Link_832175830" MODIFIED="1319472615945" TEXT="no branching allowed"/>
+<node CREATED="1319472319091" ID="Freemind_Link_1791241835" MODIFIED="1319472336023" TEXT="successor"/>
+<node CREATED="1319472336784" ID="Freemind_Link_76820386" MODIFIED="1319472340699" TEXT="predecessor"/>
+</node>
+<node CREATED="1319472534996" ID="Freemind_Link_806105976" MODIFIED="1319472542348" TEXT="Proto Language">
+<node CREATED="1319472544946" ID="Freemind_Link_1318441250" MODIFIED="1319472546447" TEXT="father"/>
+<node CREATED="1319472546674" ID="Freemind_Link_1717985613" MODIFIED="1319472548869" TEXT="children[]"/>
+<node CREATED="1319472567203" ID="Freemind_Link_57028179" MODIFIED="1319472570864" TEXT="earlierFamily"/>
+<node CREATED="1319472571106" ID="Freemind_Link_1364351820" MODIFIED="1319472574816" TEXT="finalFamily"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1319472168583" ID="Freemind_Link_820319514" MODIFIED="1319472174048" POSITION="right" TEXT="modeling the relationships">
+<node CREATED="1319472665665" ID="Freemind_Link_1503073735" MODIFIED="1319472667802" TEXT="L2L">
+<node CREATED="1319472645752" ID="Freemind_Link_1140371806" MODIFIED="1319472649454" TEXT="isSubsetOf"/>
+<node CREATED="1319472705851" ID="Freemind_Link_1570839027" MODIFIED="1319472711508" TEXT="isSupersetOf"/>
+<node CREATED="1319472715923" ID="Freemind_Link_69009497" MODIFIED="1319472719100" TEXT="isLCAof"/>
+<node CREATED="1319474520244" ID="Freemind_Link_140929498" MODIFIED="1319474528164" TEXT="is result of breakof of"/>
+<node CREATED="1319474528489" ID="Freemind_Link_1231467867" MODIFIED="1319474541082" TEXT="is earlier variety"/>
+<node CREATED="1319474541489" ID="Freemind_Link_644538969" MODIFIED="1319474545013" TEXT="is later variety"/>
+</node>
+<node CREATED="1319472671783" ID="Freemind_Link_1252486944" MODIFIED="1319472674224" TEXT="L2R">
+<node CREATED="1319472681021" ID="Freemind_Link_1120851840" MODIFIED="1319472689746" TEXT="isDescribedBy"/>
+</node>
+<node CREATED="1319472674586" ID="Freemind_Link_1718181126" MODIFIED="1319472676139" TEXT="R2L">
+<node CREATED="1319472649836" ID="Freemind_Link_1336713900" MODIFIED="1319472653646" TEXT="describes"/>
+</node>
+</node>
+<node CREATED="1319472743659" ID="Freemind_Link_126526507" MODIFIED="1319472750998" POSITION="right" TEXT="properties of predicates">
+<node CREATED="1319472752219" ID="Freemind_Link_574208840" MODIFIED="1319472756180" TEXT="reflexivity"/>
+<node CREATED="1319472756445" ID="Freemind_Link_811973353" MODIFIED="1319472759939" TEXT="symmetry"/>
+<node CREATED="1319472760187" ID="Freemind_Link_1026278884" MODIFIED="1319472763938" TEXT="transitivity"/>
+<node CREATED="1319472790900" ID="Freemind_Link_562086782" MODIFIED="1319472793146" TEXT="closures"/>
+<node CREATED="1319472793551" ID="Freemind_Link_225369105" MODIFIED="1319472802164" TEXT="Quirk does not describe Old English"/>
+</node>
+<node CREATED="1319472808339" ID="Freemind_Link_607103135" MODIFIED="1319474219799" POSITION="right" TEXT="implementation">
+<node CREATED="1319474225529" ID="Freemind_Link_470092715" MODIFIED="1319474226839" TEXT="owl"/>
+<node CREATED="1319474221417" ID="Freemind_Link_994886301" MODIFIED="1319474224902" TEXT="Glottolog website"/>
+</node>
+<node CREATED="1319472433043" ID="Freemind_Link_806661467" MODIFIED="1319472436428" POSITION="right" TEXT="conclusion"/>
+<node CREATED="1319474235909" ID="Freemind_Link_1458332072" MODIFIED="1319474239116" POSITION="right" TEXT="outlook">
+<node CREATED="1319472437439" ID="Freemind_Link_1307405263" MODIFIED="1319472445274" TEXT="extension to areal linguistics"/>
+</node>
+</node>
+</map>
---
+++ articles/current/valency/valency.mm
@@ -0,0 +1,18 @@
+<map version="0.8.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1297796736338" ID="Freemind_Link_1056256477" MODIFIED="1297796740795" TEXT="valency">
+<node CREATED="1297796812729" ID="Freemind_Link_1903000389" MODIFIED="1297796818056" POSITION="right" TEXT="synchronic analysis"/>
+<node CREATED="1297796742267" ID="_" MODIFIED="1297796744907" POSITION="right" TEXT="tesniere"/>
+<node CREATED="1297796745348" ID="Freemind_Link_948076850" MODIFIED="1297796750953" POSITION="right" TEXT="argument/adjunct">
+<node CREATED="1297796752732" ID="Freemind_Link_52433295" MODIFIED="1297796755142" TEXT="duuduk"/>
+<node CREATED="1297796755401" ID="Freemind_Link_779185125" MODIFIED="1297796760751" TEXT="nyaanyi"/>
+<node CREATED="1297796760992" ID="Freemind_Link_806719282" MODIFIED="1297796763488" TEXT="banthu"/>
+<node CREATED="1297796763726" ID="Freemind_Link_950071783" MODIFIED="1297796767446" TEXT="nyaanyi"/>
+</node>
+<node CREATED="1297796780796" ID="Freemind_Link_658612518" MODIFIED="1297796789366" POSITION="right" TEXT="semantic roles assigned by verbs"/>
+<node CREATED="1297796769497" ID="Freemind_Link_229582223" MODIFIED="1297796780409" POSITION="right" TEXT="typical case frames"/>
+<node CREATED="1297796798551" ID="Freemind_Link_1074793512" MODIFIED="1297796804424" POSITION="right" TEXT="one place"/>
+<node CREATED="1297796805129" ID="Freemind_Link_1385160419" MODIFIED="1297796807970" POSITION="right" TEXT="two place"/>
+<node CREATED="1297796808227" ID="Freemind_Link_1951376568" MODIFIED="1297796809885" POSITION="right" TEXT="three place"/>
+</node>
+</map>
