2b8dcb3eb53e6e019dbbbfc01593b55738487fff
Schmalhans <lqfb@glottotopia.de>
2015-12-04 11:21:18 +0100

    2013

---
+++ articles/2013/anju/converging to diverge.mm
@@ -0,0 +1,113 @@
+<map version="0.8.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1257261045470" ID="Freemind_Link_807252759" MODIFIED="1257261051804" TEXT="converging to diverge">
+<node CREATED="1257261052821" ID="_" MODIFIED="1257261057535" POSITION="right" TEXT="the easy way">
+<node CREATED="1257261058340" ID="Freemind_Link_516215551" MODIFIED="1257261064632" TEXT="everybody let&apos;s go tamil">
+<node CREATED="1257261065500" ID="Freemind_Link_1689759933" MODIFIED="1257261070952" TEXT="SL Tamil is archaic"/>
+<node CREATED="1257261071372" ID="Freemind_Link_977764062" MODIFIED="1257261078729" TEXT="SL Tamil has not changed a lot"/>
+<node CREATED="1257261078965" ID="Freemind_Link_1507436639" MODIFIED="1257261098017" TEXT="all other SL languages have become more Tamil in the course of time"/>
+</node>
+<node CREATED="1257261099990" ID="Freemind_Link_780000209" MODIFIED="1257261102018" TEXT="Sinhala">
+<node CREATED="1257261102326" ID="Freemind_Link_539750568" MODIFIED="1257261125747" TEXT="Sinhala is quite deviant from the Nothern Indian languages"/>
+<node CREATED="1257261126767" ID="Freemind_Link_1227159298" MODIFIED="1257261138668" TEXT="long e,o"/>
+<node CREATED="1257261139008" ID="Freemind_Link_893542639" MODIFIED="1257261150236" TEXT="no aspiration"/>
+<node CREATED="1257261150897" ID="Freemind_Link_440929065" MODIFIED="1257261154469" TEXT="relative clauses"/>
+<node CREATED="1257261154737" ID="Freemind_Link_574262507" MODIFIED="1257261156501" TEXT="evidential"/>
+<node CREATED="1257261180082" ID="Freemind_Link_1771425715" MODIFIED="1257261181806" TEXT="no copula"/>
+<node CREATED="1257262698745" ID="Freemind_Link_374618391" MODIFIED="1257262708669" TEXT="Tennant et al"/>
+<node CREATED="1257261182051" ID="Freemind_Link_1421992939" MODIFIED="1257261186991" TEXT="Elizarenkova"/>
+<node CREATED="1257261187371" ID="Freemind_Link_83677991" MODIFIED="1257261193791" TEXT="look, Tamil is just like that"/>
+</node>
+<node CREATED="1257262718290" ID="Freemind_Link_280341942" MODIFIED="1257262735551" TEXT="SL Portuguese">
+<node CREATED="1257262736452" ID="Freemind_Link_1537987125" MODIFIED="1257262748496" TEXT="unromance"/>
+<node CREATED="1257262748732" ID="Freemind_Link_1060946516" MODIFIED="1257262757864" TEXT="in contact with Tamil"/>
+<node CREATED="1257262758077" ID="Freemind_Link_1898809996" MODIFIED="1257262771801" TEXT="Tamil wives learnt Portuguese"/>
+<node CREATED="1257262772046" ID="Freemind_Link_1982439237" MODIFIED="1257262780834" TEXT="substrate influence"/>
+</node>
+<node CREATED="1257262786142" ID="Freemind_Link_1956048836" MODIFIED="1257262789762" TEXT="SL Malay">
+<node CREATED="1257262790574" ID="Freemind_Link_1346653179" MODIFIED="1257262943075" TEXT="SL Malay has  the same structure as SLP"/>
+<node CREATED="1257262943327" ID="Freemind_Link_990531351" MODIFIED="1257262949931" TEXT="ergo the same origin"/>
+<node CREATED="1257262950192" ID="Freemind_Link_307221884" MODIFIED="1257262952292" TEXT="abduction"/>
+<node CREATED="1257262955008" ID="Freemind_Link_853676463" MODIFIED="1257262972965" TEXT="Tamil wives married  Malay soldiers"/>
+<node CREATED="1257262999090" ID="Freemind_Link_197517669" MODIFIED="1257263004182" TEXT="substrate influence from Tamil"/>
+</node>
+<node CREATED="1257263009851" ID="Freemind_Link_1202200902" MODIFIED="1257263470283" TEXT="easy conclusion: all languages in SL converge towardsSri Lankan Tamil"/>
+</node>
+<node CREATED="1257262679192" ID="Freemind_Link_1165421465" MODIFIED="1257262683924" POSITION="right" TEXT="the painful way">
+<node CREATED="1257263029511" ID="Freemind_Link_1887715394" MODIFIED="1257263043089" TEXT="what Tamil are we talking about?">
+<node CREATED="1257263165724" ID="Freemind_Link_1814225274" MODIFIED="1257263173152" TEXT="&quot;Standard Tamil&quot;"/>
+<node CREATED="1257263173388" ID="Freemind_Link_743894243" MODIFIED="1257263178760" TEXT="Indian Tamil?"/>
+<node CREATED="1257263178973" ID="Freemind_Link_1332364366" MODIFIED="1257263182489" TEXT="SL Tamil?">
+<node CREATED="1257263474166" ID="Freemind_Link_1912228950" MODIFIED="1257263486866" TEXT="prestigious Jaffna Tamil"/>
+<node CREATED="1257263487230" ID="Freemind_Link_1816449516" MODIFIED="1257263491922" TEXT="generalized SL Tamil?"/>
+</node>
+</node>
+<node CREATED="1257263185597" ID="Freemind_Link_743192865" MODIFIED="1257263193105" TEXT="The ethnography of Tamil in SL">
+<node CREATED="1257263201670" ID="Freemind_Link_645116316" MODIFIED="1257263204666" TEXT="classical">
+<node CREATED="1257263194213" ID="Freemind_Link_1206831488" MODIFIED="1257263196689" TEXT="Jaffna">
+<node CREATED="1257263502239" ID="Freemind_Link_207015085" MODIFIED="1257263504963" TEXT="presitigious"/>
+<node CREATED="1257263802584" ID="Freemind_Link_56767684" MODIFIED="1257263807628" TEXT="Christian/Hindu"/>
+<node CREATED="1257263835570" ID="Freemind_Link_1417026050" MODIFIED="1257263838422" TEXT="diglossic H"/>
+</node>
+<node CREATED="1257263196901" ID="Freemind_Link_1568258671" MODIFIED="1257263199209" TEXT="Trinco">
+<node CREATED="1257263808800" ID="Freemind_Link_1215097914" MODIFIED="1257263809924" TEXT="Hindu"/>
+</node>
+<node CREATED="1257263199414" ID="Freemind_Link_524555443" MODIFIED="1257263201449" TEXT="Batti">
+<node CREATED="1257263506287" ID="Freemind_Link_1965400604" MODIFIED="1257263508939" TEXT="archaic"/>
+<node CREATED="1257263811833" ID="Freemind_Link_703516275" MODIFIED="1257263816981" TEXT="Hindu/Muslim"/>
+</node>
+</node>
+<node CREATED="1257263210542" ID="Freemind_Link_303651883" MODIFIED="1257263216794" TEXT="new">
+<node CREATED="1257263219271" ID="Freemind_Link_182665213" MODIFIED="1257263224971" TEXT="Colombo"/>
+<node CREATED="1257263225231" ID="Freemind_Link_1700236539" MODIFIED="1257263236860" TEXT="generalized Tamil"/>
+</node>
+<node CREATED="1257263237856" ID="Freemind_Link_385322220" MODIFIED="1257263245540" TEXT="deviant">
+<node CREATED="1257263246240" ID="Freemind_Link_1643743903" MODIFIED="1257263247804" TEXT="Negombo"/>
+</node>
+<node CREATED="1257263248865" ID="Freemind_Link_1737704596" MODIFIED="1257263256293" TEXT="Muslim">
+<node CREATED="1257263257033" ID="Freemind_Link_1924507163" MODIFIED="1257263259141" TEXT="NE">
+<node CREATED="1257263259849" ID="Freemind_Link_316207251" MODIFIED="1257263263117" TEXT="conservative"/>
+</node>
+<node CREATED="1257263263769" ID="Freemind_Link_395182440" MODIFIED="1257263265317" TEXT="SW">
+<node CREATED="1257263265665" ID="Freemind_Link_116845551" MODIFIED="1257263272222" TEXT="convergence towards Sinhala"/>
+</node>
+</node>
+<node CREATED="1257263321869" ID="Freemind_Link_1569027189" MODIFIED="1257263324377" TEXT="immigrant Tamil">
+<node CREATED="1257263325621" ID="Freemind_Link_1028269355" MODIFIED="1257263329441" TEXT="estate Tamil"/>
+</node>
+</node>
+<node CREATED="1257262688377" ID="Freemind_Link_401696344" MODIFIED="1257262689732" TEXT="Sinhala">
+<node CREATED="1257261194035" ID="Freemind_Link_1266170937" MODIFIED="1257263287206" TEXT="Gair: against Elizarenkova"/>
+<node CREATED="1257263288539" ID="Freemind_Link_1432471819" MODIFIED="1257263301959" TEXT="what happened, and what could we have expected to happen?"/>
+</node>
+<node CREATED="1257263339830" ID="Freemind_Link_602796519" MODIFIED="1257263343546" TEXT="SL Portuguese">
+<node CREATED="1257263344318" ID="Freemind_Link_1779797049" MODIFIED="1257263385980" TEXT="stands as is as converged towards conservative varieties"/>
+</node>
+<node CREATED="1257263347726" ID="Freemind_Link_1994171573" MODIFIED="1257263352002" TEXT="SL Malay">
+<node CREATED="1257263614629" ID="Freemind_Link_1786930134" MODIFIED="1257263625234" TEXT="Sinhala and Tamil share many structures"/>
+<node CREATED="1257263585684" ID="Freemind_Link_1699972081" MODIFIED="1257263610561" TEXT="Sinhala influence on SLM can be called Dravidian influence through the back door"/>
+<node CREATED="1257263352742" ID="Freemind_Link_1658074038" MODIFIED="1257263372987" TEXT="no convergence towards conservative Tamil varieties bcs of geography"/>
+<node CREATED="1257263872004" ID="Freemind_Link_148293968" MODIFIED="1257263883136" TEXT="Hindu or Christian prestige do not count for Malays"/>
+<node CREATED="1257263389377" ID="Freemind_Link_152121169" MODIFIED="1257263407533" TEXT="either generalized Tamil, Negombo Tamil, or SW Muslim Tamil"/>
+<node CREATED="1257263410346" ID="Freemind_Link_1504907118" MODIFIED="1257263425582" TEXT="NegT and SLMSWT have already converged towards Sinhala"/>
+<node CREATED="1257263633790" ID="Freemind_Link_948602882" MODIFIED="1257263636442" TEXT="So:">
+<node CREATED="1257263637463" ID="Freemind_Link_1947772820" MODIFIED="1257263721111" TEXT="NegT and SLMSWT influence on SLM is Sinhala influence through the back door"/>
+<node CREATED="1257263658436" ID="Freemind_Link_305855350" MODIFIED="1257263662876" TEXT="cascading">
+<node CREATED="1257263663640" ID="Freemind_Link_1402287591" MODIFIED="1257263672892" TEXT="Sinhala goes Dravidian">
+<node CREATED="1257263673553" ID="Freemind_Link_1771257595" MODIFIED="1257263681445" TEXT="Tamil varieties go Sinhala">
+<node CREATED="1257263682201" ID="Freemind_Link_1013671819" MODIFIED="1257263693558" TEXT="SLM goes Sinhalized Tamil"/>
+<node CREATED="1257263693770" ID="Freemind_Link_349873538" MODIFIED="1257263700054" TEXT="how to tell this apart?"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1257263916303" ID="Freemind_Link_165370667" MODIFIED="1257263921571" POSITION="right" TEXT="overall conclusion">
+<node CREATED="1257263922327" ID="Freemind_Link_985145790" MODIFIED="1257263943524" TEXT="at bird&apos;s eye view, Sinhala goes Tamil"/>
+<node CREATED="1257263944000" ID="Freemind_Link_715082696" MODIFIED="1257263969501" TEXT="at closer view, Sinhala goes Dravidian, but Tamil varieties do all kinds of things"/>
+<node CREATED="1257263978250" ID="Freemind_Link_931306559" MODIFIED="1257263990831" TEXT="Malay is in the middle of the confusion"/>
+<node CREATED="1257264010676" ID="Freemind_Link_1108829869" MODIFIED="1257264036609" TEXT="linear convergence is not a viable alternative since we are dealing with a moving target"/>
+</node>
+</node>
+</map>
